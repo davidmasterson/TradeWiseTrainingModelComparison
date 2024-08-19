4fbda2294275df4d2929382bc6efce8e0edf908d
david <david73180@gmail.com>
2024-08-18 23:14:00 -0500

    Removed LSTM trainer.py

--- LSTM_Trainer.py
+++
@@ -1,7 +0,0 @@
-import alpaca_request_methods
-from database import transactions_DAOIMPL
-import subprocess
-
-alpaca_request_methods.fetch_stock_data()
-model_and_report = subprocess.run(['python', 'model_generator.py'],capture_output=True)
-
