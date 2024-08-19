1cfc52a587eba233dc0485d2abda6b1be894ba4d
david <david73180@gmail.com>
2024-08-18 23:13:44 -0500

    Created a prediction route and html page

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from alpaca_request_methods import fetch_stock_data
-from LSTM_Trainer import train_lstm
+import alpaca_request_methods
 import threading
 import model_trainer_predictor_methods
 
@@ -14,20 +13,24 @@ predictions = []
 def index():
     return render_template('index.html')
 
-@app.route('/create_rf_model', methods=['POST'])
+@app.route('/create_rf_model', methods=['GET'])
 def create_rf_model():
     global progress
     model_trainer_predictor_methods.model_trainer()
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    global progress
+    probs = model_trainer_predictor_methods.stock_predictor_using_pretrained_model()
+    print(probs)
+    return render_template('stock_predictions.html', probs=probs)
 
 @app.route('/progress')
 def get_progress():
     global progress
     return jsonify({"progress": progress})
 
-@app.route('/predictions')
-def get_predictions():
-    global predictions
-    return jsonify(predictions)
 
 if __name__ == '__main__':
     app.run(debug=False)
