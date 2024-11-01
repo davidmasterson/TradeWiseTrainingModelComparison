8ace7cdc3e57e939c2f14b944085d4e0f511c209
davidmasterson <david73180@gmail.com>
2024-10-31 21:23:55 -0400

    modified the flask price and sell updater that runs in the background.

--- Seller/tasks.py
+++ Seller/tasks.py
@@ -7,31 +7,30 @@ from database import transactions_DAOIMPL, database_connection_utility, pending_
 import alpaca_request_methods
 from flask import session
 import order_methods
-from pytz import timezone
+
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def check_positions_in_background(username, user_id):
-    today = date.today()
-    conn = alpaca_request_methods.create_alpaca_api(username)
-    est = timezone('America/New_York')  # Define EST timezone
     
-    # Make sure to open the DB connection only once
+    connection = alpaca_request_methods.create_alpaca_api(username)
+    # open the DB connection only once
     db_conn = database_connection_utility.get_db_connection()
     
     try:
         while True:
             # Check if current time in EST is past 3:30 PM
-            current_time_est = datetime.now(est)
-            if current_time_est >= current_time_est.replace(hour=15, minute=30, second=0, microsecond=0):
+            now = datetime.now()
+            if now >= datetime(now.year,now.month,now.day,15,30,00):
                 print("Market close reached. Exiting position monitoring.")
                 break
             
             # Retrieve and process positions
-            poss = conn.list_poss()
+            poss = connection.list_positions()
             if poss:
                 for pos in poss:
+                    print(pos.symbol)
                     transact = transactions_DAOIMPL.get_open_transactions_for_user_by_symbol_with_db_conn(pos.symbol, user_id, db_conn)
                     if transact:
                         for trans in transact:
@@ -47,14 +46,16 @@ def check_positions_in_background(username, user_id):
                 print("No open positions found.")
             
             # Log the loop iteration time
-            print(f"Loop iteration at: {current_time_est}")
+            print(f"Loop iteration at: {datetime.now()}")
             
             # Wait for 60 seconds before checking positions again
             time.sleep(60)
+    except Exception as e:
+        print(e)
 
     finally:
         # After exiting the loop, cancel any open orders and truncate pending orders table
-        conn.cancel_all_orders()
+        connection.cancel_all_orders()
         pending_orders_DAOIMPL.truncate_pending_orders_at_eod(db_conn)
         db_conn.close()
         print("End of day: All open orders canceled and pending orders truncated.")
