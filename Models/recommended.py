d2d5a6726f29f60173c29851eec2550eb7e49578
davidmasterson <david73180@gmail.com>
2024-11-10 03:01:17 -0500

    added a user_id parameter to distringuish between user recommnedations

--- Models/recommended.py
+++ Models/recommended.py
@@ -4,7 +4,8 @@
 class Recommended:
     
     
-    def __init__(self, symbol, price, confidence):
+    def __init__(self, symbol, price, confidence, user_id):
         self.symbol = symbol
         self.price = price
         self. confidence = confidence
+        self.user_id = user_id
