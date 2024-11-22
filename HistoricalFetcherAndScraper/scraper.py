0871a30b391155938e540a19991959c59c9abfce
davidmasterson <david73180@gmail.com>
2024-11-22 13:19:06 -0500

    changed way of retrieving historical stock sentiment by using Alpaca news api

--- HistoricalFetcherAndScraper/scraper.py
+++ HistoricalFetcherAndScraper/scraper.py
@@ -5,36 +5,30 @@ import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import random
 import urllib
+from database import user_DAOIMPL
+
+from datetime import date, timedelta, datetime
 
 
 
-def search(required_keywords = None, encoded_query = None,current_page=None, ):
-    if required_keywords:
-        query = build_query(required_keywords)
-        encoded_query = urllib.parse.quote(query)
-    filtered_links = []
-    # Correct pagination parameter
-    target_url = f"https://www.bing.com/search?q={encoded_query}&rdr=1&first={current_page * 10}&setmkt=en-US&setlang=en"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Connection": "keep-alive",
-    }
-    response = requests.get(target_url, headers=headers)
-    if response.status_code != 200:
-        print(f"Failed to fetch Bing results. Status code: {response.status_code}")
-        return
-    response_text = response.text
-    soup = BeautifulSoup(response_text, 'html.parser')
-    completeData = soup.find_all("li",{"class":"b_algo"})
-    for i in range(0, len(completeData)):
-        curlink =completeData[i].find("a")['href']
-        filtered_links.append(curlink)
-    time.sleep(2)  # Add delay between requests
-    current_page += 1
-    return [filtered_links, encoded_query, current_page]
+def search(date_requirement, symbol, user_id):
+    import requests
+    start = datetime.strptime(date_requirement, "%Y-%M-%d") - timedelta(days=3) 
+    start = start.strftime("%Y-%M-%d")
+    end = date_requirement
+    
+    url = f"https://data.alpaca.markets/v1beta1/news?start={start}&end={end}&sort=desc&symbols={symbol}&limit=10"
+    user = user_DAOIMPL.get_user_by_user_id(user_id)
+    if user:
+        u_alp_k = user[6]
+        u_alp_s_k = user[7]
+        
+
+    headers = {"accept": "application/json",
+               "APCA-API-KEY-ID": u_alp_k,
+                "APCA-API-SECRET-KEY": u_alp_s_k}
+    response = requests.get(url, headers=headers)
+    return response.text
 
 
 
@@ -61,25 +55,25 @@ def get_sentiment_scores(text):
         scores = analyzer.polarity_scores(text)
         return scores['neg'], scores['neu'], scores['pos']
 
-def get_filtered_links(soup, required_keywords):
+# def get_filtered_links(soup, required_keywords):
     
     
-    filtlinks = []  # To store filtered external links
-    with open('results.txt','w') as writer:
-        writer.write(soup.text)
-        writer.close()
-    # Bing result links are in <a> tags
+#     filtlinks = []  # To store filtered external links
+#     with open('results.txt','w') as writer:
+#         writer.write(soup.text)
+#         writer.close()
+#     # Bing result links are in <a> tags
     
-        print(curlink)
-        if (not 'thread' in curlink and not 'wikipedia' in curlink and not 'store' in curlink and not 'discussion' in curlink and not 'support' in curlink):
-            print(f"Adding filtered link: {curlink}")
-            filtlinks.append(curlink)
-    return filtlinks
+#         print(curlink)
+#         if (not 'thread' in curlink and not 'wikipedia' in curlink and not 'store' in curlink and not 'discussion' in curlink and not 'support' in curlink):
+#             print(f"Adding filtered link: {curlink}")
+#             filtlinks.append(curlink)
+#     return filtlinks
 
 
 
-def build_query(required_keywords):
-    return " ".join(required_keywords)
+# def build_query(required_keywords):
+#     return " ".join(required_keywords)
 
 
 
@@ -116,3 +110,5 @@ def filter_results_by_date(links, target_date):
             print(f"Error processing {link}: {e}")
     
     return matching_links, links
+
+
