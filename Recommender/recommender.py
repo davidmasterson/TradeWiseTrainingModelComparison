b2b8059d67005ada715daf3b81f2900dd19e0f8b
davidmasterson <david73180@gmail.com>
2024-12-20 19:24:32 -0500

    removed unused imports

--- Recommender/recommender.py
+++ Recommender/recommender.py
@@ -1,69 +1,19 @@
-from Hypothetical_Predictor import CSV_Writer, stock_data_fetcher, predict_with_pre_trained_model
-import subprocess
+from Hypothetical_Predictor import CSV_Writer
 import logging
-from database import models_DAOIMPL, progression_DAOIMPL
-from MachineLearningModels import manual_alg_requisition_script
+from database import  progression_DAOIMPL
 from datetime import datetime
 
 
 
 
-# def get_model_recommendation(stock_list, count=0, iter=0, symbols=None, max_symbols=20):
-    
-#     if symbols is None:
-        
-#         symbols = []  # Initialize symbols as an empty list if not provided
-        
-#     # Process the stock list in chunks
-#     while len(symbols) < max_symbols:
-       
-#         # Get the current chunk (50 stocks at a time)
-#         this_iter_list = stock_list[iter * 50: (iter + 1) * 50]
-#         if not this_iter_list:  # Exit if no more stocks are left
-#             break
-
-#         # Write temporary CSV file with stock data
-#         # CSV_Writer.CSV_Writer.write_temporary_csv(this_iter_list)
-
-#         # Fetch the stock data
-        
-              
-
-
-#         # Run preprocessing
-
-#         # Get predictions from the model
-#         probs = manual_alg_requisition_script.get_positions_to_buy(this_iter_list)
-
-
-#         # Open the file in append mode after the first iteration
-        
-#         for prob in probs:
-#             # Check if the prediction meets the condition (prob[-1] == 1)
-            
-#             symbols.append(prob)  # Append the symbol to the list
-#             if len(symbols) >= max_symbols:  # Stop when we have enough symbols
-#                 logging.info(symbols)
-#                 return symbols
-
-#         # Update iteration and count for the next loop
-#         iter += 1
-#         count += 50
-#         progress_now = 50 + (2 * len(symbols))
-#         progress = progression_DAOIMPL.get_recommender_progress_by_user(user_id)
-#         progression_DAOIMPL.update_recommender_progress(progress_now,progress[0])
-
-#     logging.info('Finished processing symbols:', symbols)
-#     return symbols
 
 
 def get_model_recommendations_for_recommender(new_list,recommendations_script_id, dataset_id, user_id, 
-                                              max_total_spend,recommendation_count, progression = 20, count=0, iter=0, symbols=None):
-    import pickle
-    import tempfile
-    from MachineLearningModels import MARecommender
+                                              max_total_spend,recommendation_count,model_id, progression = 20, count=0, iter=0, symbols=None):
+    
+    
     from Models import progress_object, recommendation_script
-    from database import recommendation_scripts_DAOIMPL
+    
     import pandas as pd
     
     
@@ -116,7 +66,7 @@ def get_model_recommendations_for_recommender(new_list,recommendations_script_id
             # Run preprocessing
 
             # Get predictions from the model
-            probs = recommendation_script.RecommendationScript.retrainer_for_recommender(recommendations_script_id,project_root, user_id, dataset_id)
+            probs = recommendation_script.RecommendationScript.retrainer_for_recommender(recommendations_script_id,project_root, user_id, dataset_id, model_id)
 
 
             # Open the file in append mode after the first iteration
